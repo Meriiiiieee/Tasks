5fd33a51dc705d867643058da59f70e0952bdffc
Meriiiiieee <meri.hakobyan21@edu.ysu.am>
2025-09-08 04:37:06 -0700

    4

---
+++ symbol.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define SIZE 5
+
+int main() {
+	char arr[SIZE];
+	
+	printf("Enter the symbols: ");
+	for(int i = 0; i<SIZE; i++) {
+	    scanf("%c", &arr[i]);
+	}
+
+	for(int i = 0; i<SIZE; i++) {
+	    if (arr[i]>='0' && arr[i]<='9') {
+ 	 	printf("%c", arr[i]);
+	    }
+	}
+
+return 0;
+}
